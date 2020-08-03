40e133f07ed74ee9134a8eabe7278dceaa93736b
Kringelum2 <p.kringelum@gmail.com>
2020-08-03 20:38:54 +0200

    Initial commit

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,20 @@
+var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Teacher";
+console.log(job);
+
+var _3years = 3;
+var johnMark = "john and Mark";
+
+var is10higher = 10 > 48;
+console.log("er 10 højere end 48? " + is10higher);
